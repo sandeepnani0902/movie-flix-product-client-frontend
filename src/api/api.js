42cca9dcbe7285a8f7577cb72384b5960bbab32e
sandeepnani0902 <sandeepnani0902@gmail.com>
2026-05-25 15:05:04 +0530

    feat: configure axios instance with base URL and auth interceptors for API requests

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2025/api",
+  baseURL: "https://movie-flix-product-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
